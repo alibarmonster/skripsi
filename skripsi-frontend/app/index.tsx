d347b3794eb89c5d64221f577babdca516a86b3b
kalimonster <kalimonster@kali.net>
2025-05-16 05:07:54 +0100

    init: setup awal frontend

---
+++ skripsi-frontend/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const Index = () => {
+  return (
+    <View className='flex-1 justify-center align-middle'>
+      <Text className='font-semibold text-7xl color-blue-800'>alibar gagah</Text>
+    </View>
+  );
+};
+
+export default Index;
